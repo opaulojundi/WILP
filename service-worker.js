be0fa88f406abc5520a4c61bba444a62dd578db5
opaulojundi <135652382+opaulojundi@users.noreply.github.com>
2025-11-26 02:32:55 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,13 @@
-const CACHE_NAME = 'wilp-cache-v1';
+const CACHE_NAME = 'fiancailles-cache-v1';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './style.css',
+  './IMG_20250908-WA0017.png',
+  './IMG_20250908-WA0017.png',
   './script.js',
-  './IMG-20250908-WA0017.png',
+  './style.css',
+  './generate-icons.php',
+  './WhatsApp_Image_2025-09-08_at_17.41.44-removebg-preview.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap',
