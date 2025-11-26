3f8ae964634d6f2329f32f3b563ad3d9d269a4e7
opaulojundi <135652382+opaulojundi@users.noreply.github.com>
2025-11-26 02:47:35 -0300

    Update generate-icons.php

--- generate-icons.php
+++ generate-icons.php
@@ -5,7 +5,7 @@ $sizes = [72, 96, 128, 144, 152, 192, 384, 512];
 foreach ($sizes as $size) {
     $image = imagecreatefrompng($sourceImage);
     $resized = imagescale($image, $size, $size);
-    imagepng($resized, "icons/icon-{$size}x{$size}.png");
+    imagepng($resized, "icon-{$size}x{$size}.png");
     imagedestroy($image);
     imagedestroy($resized);
 }
