bec683b50fe874ca9fe743a0bf0fa777a193f0cb
opaulojundi <135652382+opaulojundi@users.noreply.github.com>
2025-11-26 02:33:39 -0300

    Add files via upload

---
+++ generate-icons.php
@@ -0,0 +1,13 @@
+<?php
+$sourceImage = 'IMG-20250908-WA0017.png';
+$sizes = [72, 96, 128, 144, 152, 192, 384, 512];
+
+foreach ($sizes as $size) {
+    $image = imagecreatefrompng($sourceImage);
+    $resized = imagescale($image, $size, $size);
+    imagepng($resized, "icons/icon-{$size}x{$size}.png");
+    imagedestroy($image);
+    imagedestroy($resized);
+}
+echo "Ícones gerados";
+?>
